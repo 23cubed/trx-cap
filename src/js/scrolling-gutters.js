3b172e12604574bb90d6d6ee642c9ac77f4ff497
bdc3 <ben@23cubed.com>
2025-07-07 01:11:29 -0400

    test: Trigger simplified workflow with @main CDN purging
    - Small change to test cache purging functionality
    - Verify @main branch CDN updates immediately

--- src/js/scrolling-gutters.js
+++ src/js/scrolling-gutters.js
@@ -1,6 +1,7 @@
 // Scrolling gutters module
 // TODO: Add scrolling gutters functionality
 // Test: Auto-versioning system
+// Test: @main branch with cache purging
 
 export function initScrollingGutters() {
   // Implementation coming soon
