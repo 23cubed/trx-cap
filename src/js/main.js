04c5701611ba21ecdd83b53ff67397f204a6abac
bdc3 <actions@github.com>
2025-07-07 03:38:35 -0400

    Add pre-commit hook for generate-imports

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,4 @@ import './split-text.js';
 
 // Simple confirmation that modules loaded
 console.log('🚀 TRX Cap modules loaded:', ['./hero.js', './navbar.js', './page-transition.js', './scrolling-gutters.js', './split-text-on-scroll.js', './split-text.js']);
-console.log('📅 Updated at 7/7/2025, 3:30:23 AM');
+console.log('📅 Updated at 7/7/2025, 3:38:35 AM');
