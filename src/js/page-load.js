212d3e8af03c2c09860b2ca1c63035fba12cb716
bdc3 <actions@github.com>
2025-07-13 16:58:48 -0400

    Update page-load.js

--- src/js/page-load.js
+++ src/js/page-load.js
@@ -4,6 +4,7 @@ function pageLoader() {
     tl.to(".page-loader", {
         opacity: 0,
         duration: 0.8,
+        delay: 0.2,
         ease: "power4.out",
         onComplete: () => {
             gsap.set(".page-loader", { visibility: "hidden" });
