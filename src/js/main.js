658ab7d805f1dfa3b5dcdf425250496483f9968a
bdc3 <actions@github.com>
2025-07-17 00:01:49 -0400

    Refine build

--- src/js/main.js
+++ src/js/main.js
@@ -44,5 +44,5 @@ window.initSplitText = initSplitText;
 
 // Simple confirmation that modules loaded
 console.log('🚀 TRX Cap modules loaded:', ['./formErrors.js', './greenSection.js', './hero.js', './navbar.js', './page-load.js', './page-transition.js', './portfolio-grid.js', './scrolling-gutters.js', './split-text-on-scroll.js', './split-text.js', './three-js-module.js']);
-console.log('📅 Updated at 7/16/2025, 11:52:28 PM EST');
+console.log('📅 Updated at 7/17/2025, 12:01:49 AM EST');
 console.log('🚀 Functions available for import:', ['initFormErrors', 'animSectionsOnScroll', 'initHero', 'animateHeroCTA', 'initHeader', 'setScrolled', 'pageLoader', 'initPortfolioGrid', 'initScrollingGutters', 'initTextScroll', 'splitTextElement', 'animateSplitText', 'initSplitText']);
--- src/js/three-js-module.js
+++ src/js/three-js-module.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { MeshSurfaceSampler } from '../../node_modules/three/examples/jsm/math/MeshSurfaceSampler.js';
-import { WebGPURenderer } from '../../node_modules/three/examples/jsm/renderers/WebGPURenderer.js';
-import { PointsNodeMaterial } from '../../node_modules/three/examples/jsm/nodes/materials/PointsNodeMaterial.js';
+import { WebGPURenderer } from '../../node_modules/three/build/three.webgpu.js';
+import { PointsNodeMaterial } from '../../node_modules/three/build/three.webgpu.nodes.js';
 
 window.THREE = THREE;
 window.GLTFLoader = GLTFLoader;
