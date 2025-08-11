a8639a275156270c1801737267dd5f5d5e81a380
bdc3 <actions@github.com>
2025-08-10 23:40:25 -0400

    file clean up

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@
 // Run 'npm run generate-imports' to update
 
 import './formErrors.js';
-import './greenSection.js';
 import './hero.js';
 import './navbar.js';
 import './page-load.js';
@@ -10,31 +9,26 @@ import './page-transition.js';
 import './particle-hero-mesh-morph.js';
 import './particle-icons.js';
 import './particle-texture.js';
-import './scrolling-gutters.js';
 import './split-text-on-scroll.js';
 import './split-text.js';
-import './text-read-more.js';
 import './three-js-module.js';
 
 // Import functions to re-export for ES6 module usage
 import { initFormErrors } from './formErrors.js';
-import { animSectionsOnScroll } from './greenSection.js';
 import { initHero, animateHeroCTA } from './hero.js';
 import { initNavbar, setScrolled } from './navbar.js';
 import { pageLoader } from './page-load.js';
 import { initParticleHeroMeshMorph, disposeParticleHeroMeshMorph } from './particle-hero-mesh-morph.js';
 import { initParticleIcon } from './particle-icons.js';
 import { InitParticleTexture } from './particle-texture.js';
-import { initScrollingGutters } from './scrolling-gutters.js';
 import { initTextScroll } from './split-text-on-scroll.js';
 import { splitTextElement, animateSplitText, initSplitText } from './split-text.js';
 
 // Re-export all functions for ES6 imports
-export { initFormErrors, animSectionsOnScroll, initHero, animateHeroCTA, initNavbar, setScrolled, pageLoader, initParticleHeroMeshMorph, disposeParticleHeroMeshMorph, initParticleIcon, InitParticleTexture, initScrollingGutters, initTextScroll, splitTextElement, animateSplitText, initSplitText };
+export { initFormErrors, initHero, animateHeroCTA, initNavbar, setScrolled, pageLoader, initParticleHeroMeshMorph, disposeParticleHeroMeshMorph, initParticleIcon, InitParticleTexture, initTextScroll, splitTextElement, animateSplitText, initSplitText };
 
 // Also expose functions globally for simple script usage
 window.initFormErrors = initFormErrors;
-window.animSectionsOnScroll = animSectionsOnScroll;
 window.initHero = initHero;
 window.animateHeroCTA = animateHeroCTA;
 window.initNavbar = initNavbar;
@@ -44,13 +38,12 @@ window.initParticleHeroMeshMorph = initParticleHeroMeshMorph;
 window.disposeParticleHeroMeshMorph = disposeParticleHeroMeshMorph;
 window.initParticleIcon = initParticleIcon;
 window.InitParticleTexture = InitParticleTexture;
-window.initScrollingGutters = initScrollingGutters;
 window.initTextScroll = initTextScroll;
 window.splitTextElement = splitTextElement;
 window.animateSplitText = animateSplitText;
 window.initSplitText = initSplitText;
 
 // Simple confirmation that modules loaded
-console.log('🚀 TRX Cap modules loaded:', ['./formErrors.js', './greenSection.js', './hero.js', './navbar.js', './page-load.js', './page-transition.js', './particle-hero-mesh-morph.js', './particle-icons.js', './particle-texture.js', './scrolling-gutters.js', './split-text-on-scroll.js', './split-text.js', './text-read-more.js', './three-js-module.js']);
-console.log('📅 Updated at 8/10/2025, 11:28:43 PM EST');
-console.log('🚀 Functions available for import:', ['initFormErrors', 'animSectionsOnScroll', 'initHero', 'animateHeroCTA', 'initNavbar', 'setScrolled', 'pageLoader', 'initParticleHeroMeshMorph', 'disposeParticleHeroMeshMorph', 'initParticleIcon', 'InitParticleTexture', 'initScrollingGutters', 'initTextScroll', 'splitTextElement', 'animateSplitText', 'initSplitText']);
+console.log('🚀 TRX Cap modules loaded:', ['./formErrors.js', './hero.js', './navbar.js', './page-load.js', './page-transition.js', './particle-hero-mesh-morph.js', './particle-icons.js', './particle-texture.js', './split-text-on-scroll.js', './split-text.js', './three-js-module.js']);
+console.log('📅 Updated at 8/10/2025, 11:40:26 PM EST');
+console.log('🚀 Functions available for import:', ['initFormErrors', 'initHero', 'animateHeroCTA', 'initNavbar', 'setScrolled', 'pageLoader', 'initParticleHeroMeshMorph', 'disposeParticleHeroMeshMorph', 'initParticleIcon', 'InitParticleTexture', 'initTextScroll', 'splitTextElement', 'animateSplitText', 'initSplitText']);
