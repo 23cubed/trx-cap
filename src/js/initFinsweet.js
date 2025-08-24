2d9a40e72fd3535f91e48f35f48bea7bec80caa8
fedegpo <fedegrippo01@gmail.com>
2025-08-24 14:02:05 -0300

    initFinsweet

--- src/js/initFinsweet.js
+++ src/js/initFinsweet.js
@@ -1,21 +1,20 @@
 function initFinsweet() {
     window.fsAttributes = window.fsAttributes || [];
-    // fs-list
-    window.fsAttributes.push(['fs-list']);
-    // fs-readtime
-    window.fsAttributes.push(['fs-readtime']);
-    // fs-inject
-    window.fsAttributes.push(['fs-inject']);
-    // fs-socialshare
-    window.fsAttributes.push(['fs-socialshare']);
-    // fs-copyclip
-    window.fsAttributes.push(['fs-copyclip']);
-    // fs-toc
-    window.fsAttributes.push(['fs-toc']);
-    // fs-scrolldisable
-    window.fsAttributes.push(['fs-scrolldisable']);
-  
-  }
-  
+
+    const attrs = [
+        'fs-list',
+        'fs-readtime',
+        'fs-inject',
+        'fs-socialshare',
+        'fs-copyclip',
+        'fs-toc',
+        'fs-scrolldisable'
+    ];
+
+    attrs.forEach(attr => {
+        window.fsAttributes.push([attr, null]);
+    });
+}
+
   export { initFinsweet };
   
