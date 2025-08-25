42613ced0068a93f00878f64362c15f8bb2889e0
fedegpo <fedegrippo01@gmail.com>
2025-08-25 20:52:56 -0300

    copied-message.js

--- src/js/copied-message.js
+++ src/js/copied-message.js
@@ -1,22 +1,28 @@
-function initCopied(){
-    const copyTextEl = document.getElementById("copy-text");
-    if (copyTextEl) {
-      copyTextEl.textContent = window.location.href;
-    }
+function initCopied() {
+  const copyTextEl = document.getElementById("copy-text");
+  if (copyTextEl) {
+    copyTextEl.textContent = window.location.href;
+  }
 
-    const copyButtons = document.querySelectorAll('[fs-copyclip-element="click"], [fs-copyclip-element="click-2"]');
-    const copiedMessage = document.querySelector(".copied-message");
+  const copyButtons = document.querySelectorAll(
+    '[fs-copyclip-element="click"], [fs-copyclip-element="click-2"]'
+  );
+  const copiedMessage = document.querySelector(".copied-message");
 
-    copyButtons.forEach(function (el) {
-      el.addEventListener("click", function () {
+  copyButtons.forEach(function (el) {
+    el.addEventListener("click", function () {
+      navigator.clipboard.writeText(window.location.href).then(() => {
         if (copiedMessage) {
           copiedMessage.classList.remove("hide");
           setTimeout(function () {
             copiedMessage.classList.add("hide");
           }, 2000);
         }
+      }).catch(err => {
+        console.error("Error", err);
       });
     });
+  });
 }
 
 export { initCopied };
