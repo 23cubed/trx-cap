db2e750cf88781fb919d4519e507673855b9c5d4
fedegpo <fedegrippo01@gmail.com>
2025-08-25 21:06:14 -0300

    copied-message.js

--- src/js/copied-message.js
+++ src/js/copied-message.js
@@ -1,25 +1,27 @@
 function initCopied() {
-  const copyTextEl = document.getElementById("copy-text");
-  if (copyTextEl) {
-    copyTextEl.textContent = window.location.href;
-  }
+  requestAnimationFrame(() => {
+    const copyTextEl = document.getElementById("copy-text");
+    if (copyTextEl) {
+      copyTextEl.textContent = window.location.href;
+    }
 
-  const copyButtons = document.querySelectorAll(
-    '[fs-copyclip-element="click"], [fs-copyclip-element="click-2"]'
-  );
-  const copiedMessage = document.getElementById("message");
+    const copyButtons = document.querySelectorAll(
+      '[fs-copyclip-element="click"], [fs-copyclip-element="click-2"]'
+    );
+    const copiedMessage = document.getElementById("message");
 
-  copyButtons.forEach(function (el) {
-    el.addEventListener("click", function () {
-      navigator.clipboard.writeText(window.location.href).then(() => {
-        if (copiedMessage) {
-          copiedMessage.classList.remove("hide");
-          setTimeout(function () {
-            copiedMessage.classList.add("hide");
-          }, 2000);
-        }
-      }).catch(err => {
-        console.error("Error", err);
+    copyButtons.forEach(function (el) {
+      el.addEventListener("click", function () {
+        navigator.clipboard.writeText(window.location.href).then(() => {
+          if (copiedMessage) {
+            copiedMessage.classList.remove("hide");
+            setTimeout(function () {
+              copiedMessage.classList.add("hide");
+            }, 2000);
+          }
+        }).catch(err => {
+          console.error("Error", err);
+        });
       });
     });
   });
